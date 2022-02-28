0bca60d4f3f88e4a84b33c01c4bba8b804fc5522
Magnus <752zombie@gmail.com>
2022-02-28 14:13:49 +0100

    first page added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
-
 app.use(express.static("public"));
-
 const fs = require("fs");
+
+const nav = fs.readFileSync("./public/components/nav.html").toString();
+const footer = fs.readFileSync("./public/components/footer.html").toString();
+
+const node = fs.readFileSync("./public/pages/node/node.html").toString();
+const nodePage = nav.replace("%%DOCUMENT_TITLE%%", "Node.js") + node + footer;
+
+app.get("/node", (req, res) => {
+    res.send(nodePage);
+});
+
 
 app.listen(8080, () => console.log("Server is running on port", 8080));
